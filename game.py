30608fb1dae69223593b52d57e5c49c3344c2e87
kat <katharinebrooks@hotmail.co.uk>
2022-04-03 18:23:23 +0100

    update decks function removes the losing card from losing players deck and adds it to winning players deck

--- game.py
+++ game.py
@@ -17,21 +17,25 @@ class Game:
         self.player1.cards += first_half
         self.player2.cards += second_half
 
-    def get_pokemon(self):
-        num = random.randint(1, 151)
-        pokemon = Pokemon(num)
-        print(pokemon.data["name"])
-        return pokemon.data["name"]
+    def update_decks(self, winning_player):
+        if winning_player == self.player1:
+            losing_card = self.player2.cards[0]
+            self.player1.cards.append(losing_card)
+            self.player2.cards.pop(0)
+        elif winning_player == self.player2:
+            losing_card = self.player1.cards[0]
+            self.player2.cards.append(losing_card)
+            self.player1.cards.pop(0)
 
     def compare_stat(self, stat):
-        player_1_stat = self.player1.cards[0][stat]
-        player_2_stat = self.player2.cards[0][stat]
-        if player_1_stat > player_2_stat:
+        player1_stat = self.player1.cards[0][stat]
+        player2_stat = self.player2.cards[0][stat]
+        if player1_stat > player2_stat:
             print("player 1 wins!")
-            return "player 1 wins"
-        elif player_1_stat < player_2_stat:
+            return "player 1 wins!"  # return self.player2.cards[0]
+        elif player1_stat < player2_stat:
             print("player 2 wins!")
-            return "player 2 wins!"
+            return "player 2 wins!"  # return self.player1.cards[0]
         else:
             print("it is a draw!")
             return "it is a draw!"
--- test_game.py
+++ test_game.py
@@ -1,36 +1,42 @@
 import pytest
 from game import Game
 import random
+from player import Player
+from deck import Deck
 
 
 @pytest.fixture
 def game():
-    """Returns a Game instance with squirtle and venusaur"""
-    return Game()
-
-
-def test_get_random_pokemon(game):
-    random.seed(10)  # means that the next randint call executed will always return 147, which is dratini pokemon
-    # print(random.randint(1, 151))
-    # game.get_pokemon
-    assert game.get_pokemon() == "dratini"
-
-
-def test_generate_hands_gives_10_cards(game):
+    """Returns a Game instance"""
+    game = Game(Player(), Player(), Deck())
+    random.seed(1)  # subbing the random here means the generate_hands will always return the same list of pokemon
     game.generate_hands()
+    return game
+
+
+def test_generate_hands_gives_10_cards_player1(game):
     assert len(game.player1.cards) == 10
 
 
-def test_generate_hands_gives_10_cards(game):
-    game.generate_hands()
+def test_generate_hands_gives_10_cards_player2(game):
     assert len(game.player2.cards) == 10
 
 
 def test_compare_pokemon_stat(game):
-    game.generate_hands()
-    # game.select_cards
-    print(game.player1.cards[0]["name"])
-    print(game.player2.cards[0]["name"])
-    print(game.player1.cards[0]["hp"])
-    print(game.player2.cards[0]["hp"])
-    assert game.compare_stat("hp") == "player 2 wins!"
+    assert (
+        game.compare_stat("hp") == "player 1 wins!"
+    )  # thanks to the random.seed, we guarantee that player 1 card is always clefairy (hp:70 ) and player 2 card is always pikachu (hp:35 )
+
+
+def test_end_turn_gives_winner_the_card(game):
+    losing_card = game.player2.cards[0]
+    game.update_decks(game.player1)
+    assert game.player1.cards[-1] == losing_card
+
+
+def test_end_turn_removes_losing_card_from_loser_deck(game):
+    losing_card = game.player2.cards[0]
+    print(losing_card)
+    print(game.player2.cards)
+    game.update_decks(game.player1)
+    assert game.player2.cards.count(losing_card) == 0
