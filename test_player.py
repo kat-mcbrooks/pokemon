e3f69e4676c3bc9370b9fef8ea45c3847a93243f
kat <katharinebrooks@hotmail.co.uk>
2022-04-03 15:35:04 +0100

    player class: can add cards to hand and increase points

--- test_player.py
+++ test_player.py
@@ -1,62 +1,18 @@
 import pytest
-from player import Player, InsufficientAmount
+from player import Player
 
-# with fixtures, each test is provided with a newly-initialized Wallet instance, and not one that has been used in another test.
+# with fixtures, each test is provided with a newly-initialized Player instance, and not one that has been used in another test.
 @pytest.fixture
-def empty_player():
-    """returns a player instance with a zero balance"""
+def player():
+    """returns a player instance"""
     return Player()
 
 
-@pytest.fixture
-def player():
-    """Returns a Player instance with a balance of 20"""
-    return Player(20)
+def test_cards_can_be_added(player):
+    player.cards += ["squirtle", "ditto"]
+    assert player.cards == ["squirtle", "ditto"]
 
 
-@pytest.mark.parametrize(
-    "earned,spent,expected",
-    [
-        (30, 10, 20),
-        (20, 2, 18),
-    ],
-)
-def test_transations(earned, spent, expected):
-    my_player = Player()
-    my_player.add_cash(earned)
-    my_player.spend_cash(spent)
-    assert my_player.balance == expected
-
-
-def test_default_initial_amount(empty_player):
-    assert empty_player.balance == 0
-
-
-def test_add_cash(player):
-    player.add_cash(10)
-    assert player.balance == 30
-
-
-def test_spend_cash(player):
-    player.spend_cash(5)
-    assert player.balance == 15
-
-
-def test_spend_cash_raises_error_if_insufficient_funds(player):
-    with pytest.raises(InsufficientAmount):
-        player.spend_cash(25)
-
-
-# def capital_case(x):
-#     if not isinstance(x, str):
-#         raise TypeError("Please provide a string arg")
-#     return x.capitalize()
-
-
-# def test_capital_case():
-#     assert capital_case("semaphore") == "Semaphore"
-
-
-# def test_raises_exception_on_non_string_arguments():
-#     with pytest.raises(TypeError):
-#         capital_case(9)
+def test_points_can_be_added(player):
+    player.points += 5
+    assert player.points == 5
