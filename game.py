6996e2368049ed6a9ad64b643e680c5fec5ab5be
kat <katharinebrooks@hotmail.co.uk>
2022-04-03 16:37:36 +0100

    game class can generate a hand of 10 cards for each player and select one card from each players hand

--- game.py
+++ game.py
@@ -1,26 +1,30 @@
-from readline import set_completion_display_matches_hook
 from pokemon import Pokemon
+from player import Player
+from deck import Deck
 import random
 
 
 class Game:
-    def __init__(self, player1, player2, deck):
+    def __init__(self, player1=Player(), player2=Player(), deck=Deck()):
         self.player1 = player1
         self.player2 = player2
         self.deck = deck
 
     def generate_hands(self):
         self.deck.add_cards()
-        first_half = self.deck[:10]
-        second_half = self.deck[10:]
-        self.player1.cards.append(first_half)
-        self.player2.cards.append(second_half)
+        first_half = self.deck.cards[:10]
+        second_half = self.deck.cards[10:]
+        self.player1.cards += first_half
+        self.player2.cards += second_half
 
     def get_pokemon(self):
         num = random.randint(1, 151)
         pokemon = Pokemon(num)
         print(pokemon.data["name"])
         return pokemon.data["name"]
+
+    def select_cards(self, idx):
+        return [self.player1.cards[idx], self.player2.cards[idx]]
 
     def compare_stat(self, stat):
         my_poke = self.get_pokemon()
--- test_game.py
+++ test_game.py
@@ -1,6 +1,5 @@
 import pytest
 from game import Game
-from unittest.mock import patch
 import random
 
 
@@ -11,10 +10,36 @@ def game():
 
 
 def test_get_random_pokemon(game):
-    random.seed(10)
+    random.seed(10)  # means that the next randint call executed will always return 147, which is dratini pokemon
     # print(random.randint(1, 151))
     # game.get_pokemon
     assert game.get_pokemon() == "dratini"
 
-    # def test_compare_pokemon_stat(squirtle, venusaur):
-    #   # assert squirtle.compare_stat(venusaur, "hp") == "venusaur wins!"
+
+def test_generate_hands_gives_10_cards(game):
+    game.generate_hands()
+    assert len(game.player1.cards) == 10
+
+
+def test_generate_hands_gives_10_cards(game):
+    game.generate_hands()
+    assert len(game.player2.cards) == 10
+
+
+def test_select_card(game):
+    game.generate_hands()
+    # print(game.player1.cards)
+    # print(game.player1.cards[0])
+    current_cards = game.select_cards(0)
+    print(current_cards)
+    assert current_cards[0]["name"] == "blastoise"
+    assert current_cards[1]["name"] == "vaporeon"
+
+
+# def test_compare_pokemon_stat(game):
+#    game.generate_hands()
+#    game.compare('hp')
+
+# def test_compare_pokemon_stat(game):
+#     game.compare_stat("hp")
+#     # assert squirtle.compare_stat(venusaur, "hp") == "venusaur wins!"
